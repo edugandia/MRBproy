79e61fd8b7da8902c37af18dfb75c38633953e1e
unknown <edugandia@hotmail.com>
2019-04-24 19:53:37 +0200

    planteamiento del CarouselAuto

--- src/components/mainPage/header/Header.jsx
+++ src/components/mainPage/header/Header.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Header.scss";
+
+import CarouselAuto from "../../shared/carouselAuto";
 
 export default class Header extends Component {
   render() {
     return (
       <header id="header-container">
-        
+        <CarouselAuto
+        secondsOfInterval={5} />
       </header>
-    )
+    );
   }
 }
--- src/components/shared/carouselAuto/CarouselAuto.jsx
+++ src/components/shared/carouselAuto/CarouselAuto.jsx
@@ -1,12 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./CarouselAuto.scss";
 
+import ListOfProyects from "../../../data/headerCarousel.json";
+
 export default class CarouselAuto extends Component {
+  constructor() {
+    super();
+    this.state = {
+      position: 0,
+      lastPosition: ListOfProyects.length - 1
+    };
+  }
+
+  componentDidMount() {
+    this.timer();
+  }
+
+  timer = () => {
+    let milisecondsInterval = this.props.secondsOfInterval * 1000;
+    setTimeout(() => {
+      this.changePosition("add");
+      this.timer();
+    }, milisecondsInterval);
+  };
+
+  changePosition = operation => {
+    let position = this.state.position;
+    if (operation === "add") {
+      position === this.state.lastPosition ? (position = 0) : position++;
+    }
+    if (operation === "substract") {
+      position === 0 ? (position = this.state.lastPosition) : position--;
+    }
+    this.setState({ ...this.state, position });
+  };
+
+  selectPosition = position => {
+    this.setState({ ...this.state, position });
+  };
+
   render() {
     return (
-      <div>
-        
+      <div className="carouselAuto-general-container">
+        <div className="carousel-controller">
+          <div className="button-container">
+            <img
+              onClick={() => this.changePosition("substract")}
+              src="./images/left-arrow.png"
+              alt="left-arrow"
+            />
+            <img
+              onClick={() => this.changePosition("add")}
+              src="./images/right-arrow.png"
+              alt="left-arrow"
+            />
+          </div>
+          <div className="image-container">
+            <img
+              src={ListOfProyects[this.state.position].image}
+              alt={`img-${this.state.position}`}
+            />
+          </div>
+          <div className="points-controller">
+            {ListOfProyects.map((proy, i) => {
+              return (
+                <div
+                  onClick={() => this.selectPosition(i)}
+                  className={`point ${this.state.position === i && "selected"}`}
+                  key={i}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
