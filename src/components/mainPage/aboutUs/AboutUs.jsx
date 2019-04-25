8b36f0b40c8e80ad18f7a36fe0f3b6fe41a3a64b
unknown <edugandia@hotmail.com>
2019-04-25 10:04:56 +0200

    AboutUs definido

--- src/components/mainPage/aboutUs/AboutUs.jsx
+++ src/components/mainPage/aboutUs/AboutUs.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import './AboutUs.scss';
+import React, { Component } from "react";
+import "./AboutUs.scss";
 
 export default class AboutUs extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <section className="about-general-container">
+        <div className="low-opacity-container">
+          <div className="about-container">
+            <h3>SOBRE NOSOTROS</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+              modi accusantium! Harum, molestiae! Odio aut expedita temporibus?
+              Maxime placeat numquam facere, nesciunt officia consequuntur
+              deleniti voluptate nam quas animi magni.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
