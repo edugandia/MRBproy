03ae225280c74b78c3b84a1e45aed41a248d3007
unknown <edugandia@hotmail.com>
2019-04-25 09:56:00 +0200

    OurServices definido

--- src/components/mainPage/ourServices/OurServices.jsx
+++ src/components/mainPage/ourServices/OurServices.jsx
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
-import './OurServices.scss';
+import React, { Component } from "react";
+import "./OurServices.scss";
+
+import ServiceBox from "./serviceBox";
 
 export default class OurServices extends Component {
   render() {
+    const services = [
+      {
+        image: "./images/proyecto-icon.png",
+        title: "Proyectos",
+        description: "Creamos proyectos a tu medida.",
+        link: "proyects-general-container"
+      },
+      {
+        image: "./images/decoramos-icon.png",
+        title: "Decoramos",
+        description: "Decoramos siguiendo las últimas tendencias",
+        link: "decoration-general-container"
+      },
+      {
+        image: "./images/carries-icon.png",
+        title: "Portes",
+        description: "Transportamos tus cosas a tu nuevo hogar",
+        link: "carries-general-container"
+      }
+    ];
     return (
-      <div>
-        
-      </div>
-    )
+      <section className="services-general-container">
+        <div className="services-container">
+          <div className="services-title">
+            <h2>NUESTROS SERVICIOS</h2>
+          </div>
+          <div className="services-box-container">
+            {services.map((service, i) => {
+              return (
+                <ServiceBox
+                  image={service.img}
+                  title={service.title}
+                  description={service.description}
+                  link={service.link}
+                  key={i}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    );
   }
 }
---
+++ src/components/mainPage/ourServices/serviceBox/ServiceBox.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./ServiceBox.scss";
+
+import scrollToId from "../../../../utils/scrollMethods";
+
+export default class ServiceBox extends Component {
+  render() {
+    return (
+      <div className="service-box-container">
+        <img src={this.props.image} alt={this.props.title} />
+        <h4>{this.props.title}</h4>
+        <p>{this.props.description}</p>
+        <div onClick={() => scrollToId(this.props.link, true)}>
+          <h5>VER MÁS</h5>
+        </div>
+      </div>
+    );
+  }
+}
