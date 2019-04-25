d6d3bbdf8f67c25db31ec1259735d016b5c7949b
unknown <edugandia@hotmail.com>
2019-04-25 10:33:10 +0200

    Proyects definido

--- src/components/mainPage/proyects/Proyects.jsx
+++ src/components/mainPage/proyects/Proyects.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './Proyects.scss';
+import ProyectsJSON from '../../../data/proyectsCarousel.json'
+
+import CarouselComplex from '../../shared/carouselComplex'
 
 export default class Proyects extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section id="proyects-general-container">
+        <CarouselComplex 
+        isReverse={false}
+        dataJSON={ProyectsJSON}
+        title="PROYECTOS"
+        />
+      </section>
     )
   }
 }
