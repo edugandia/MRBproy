2e61391c283205cdf4a19863634aede5cba53408
unknown <edugandia@hotmail.com>
2019-04-29 11:33:23 +0200

    solucionado fallo en estructura

--- src/components/mainPage/contact/contactBox/ContactBox.jsx
+++ src/components/mainPage/contact/contactBox/ContactBox.jsx
@@ -4,12 +4,10 @@ import "./ContactBox.scss";
 export default class ContactBox extends Component {
   render() {
     return (
-      <div className="contact-box">
+      <a href={this.props.link} target="_black" className="contact-box">
         <img src={this.props.image} alt={this.props.text} />
-        <a href={this.props.link} target="_black">
-          {this.props.text}
-        </a>
-      </div>
+        <p>{this.props.text}</p>
+      </a>
     );
   }
 }
